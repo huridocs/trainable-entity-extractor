f0be91f1d58d2022a1e5a877a875b9d8dadd052a
Gabo <gabriel.piles.glez@gmail.com>
2024-11-01 10:35:39 +0100

    Test setup installation

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from setuptools import setup
 
-from setuptools import setup, find_packages
+from trainable_entity_extractor.config import ROOT_PATH
 
 requirements_path = Path("requirements.txt")
 requirements = [r for r in requirements_path.read_text().splitlines() if not r.startswith("git+")]
@@ -8,11 +9,30 @@ dependency_links = [r for r in requirements_path.read_text().splitlines() if r.s
 
 PROJECT_NAME = "trainable-entity-extractor"
 
+
+def get_recursive_subfolders(origin_path, recursive_path: Path):
+    for sub_path in recursive_path.iterdir():
+        avoid_path = False
+        for text in ["test", "__pycache__", "labeled_data", "results"]:
+            if text in str(sub_path):
+                avoid_path = True
+
+        if avoid_path:
+            continue
+
+        if sub_path.is_dir():
+            yield from get_recursive_subfolders(origin_path, sub_path)
+            yield str(sub_path).replace(str(origin_path) + "/", "")
+
+
+package_path = Path(ROOT_PATH, "src", "trainable_entity_extractor").resolve()
+base_path = Path(ROOT_PATH, "src")
+
 setup(
     name=PROJECT_NAME,
-    packages=find_packages(where="src/trainable_entity_extractor"),
+    packages=["trainable_entity_extractor"] + [folder for folder in get_recursive_subfolders(base_path, package_path)],
     package_dir={"": "src"},
-    version="0.4",
+    version="0.5",
     url="https://github.com/huridocs/trainable-entity-extractor",
     author="HURIDOCS",
     description="This tool is a trainable text/PDF to entity extractor",
