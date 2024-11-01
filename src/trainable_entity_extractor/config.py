1e48ae6cdb8108de101cc2c4097201f8e15f9781
Gabo <gabriel.piles.glez@gmail.com>
2024-11-01 14:36:52 +0100

    Add graylog

--- src/trainable_entity_extractor/config.py
+++ src/trainable_entity_extractor/config.py
@@ -1,15 +1,21 @@
 import logging
+import os
 from os.path import join
 from pathlib import Path
 
+import graypy
 
 APP_PATH = Path(__file__).parent.parent.absolute()
 ROOT_PATH = Path(__file__).parent.parent.parent.absolute()
 DATA_PATH = join(ROOT_PATH, "models_data")
+GRAYLOG_IP = os.environ.get("GRAYLOG_IP")
 
 HUGGINGFACE_PATH = join(ROOT_PATH, "huggingface")
 
 handlers = [logging.StreamHandler()]
+
+if GRAYLOG_IP:
+    handlers.append(graypy.GELFUDPHandler(GRAYLOG_IP, 12201, localname="pdf_metadata_extraction"))
 
 logging.root.handlers = []
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=handlers)
