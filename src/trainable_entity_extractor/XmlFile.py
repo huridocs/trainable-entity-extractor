ecaaf62f73d8490e07c0e2c0fd51f30cc2f9e85b
Gabo <gabriel.piles.glez@gmail.com>
2024-11-14 12:04:31 +0100

    Fix xml paths

--- src/trainable_entity_extractor/XmlFile.py
+++ src/trainable_entity_extractor/XmlFile.py
@@ -6,12 +6,12 @@ from trainable_entity_extractor.data.ExtractionIdentifier import ExtractionIdent
 
 
 class XmlFile:
-    def __init__(self, extraction_identifier: ExtractionIdentifier, to_train: bool, xml_file_name: str):
+    def __init__(self, extraction_identifier: ExtractionIdentifier, to_train: bool, xml_file_name: str = ""):
         self.extraction_identifier = extraction_identifier
         self.to_train = to_train
         self.xml_file_name = xml_file_name
         self.xml_file = None
-        self.xml_folder_path = self.get_xml_folder_path(to_train)
+        self.xml_folder_path = self.get_xml_folder_path()
         self.xml_file_path = join(self.xml_folder_path, self.xml_file_name)
 
     def save(self, file: bytes):
@@ -19,8 +19,8 @@ class XmlFile:
         file_path = Path(f"{self.xml_folder_path}/{self.xml_file_name}")
         file_path.write_bytes(file)
 
-    def get_xml_folder_path(self, to_train: bool) -> str:
-        if to_train:
+    def get_xml_folder_path(self) -> str:
+        if self.to_train:
             return join(self.extraction_identifier.get_path(), "xml_to_train")
 
         return join(self.extraction_identifier.get_path(), "xml_to_predict")
