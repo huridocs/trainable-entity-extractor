7ca2d416b3e1ed9c050d504a747ec2db4dc4764f
Gabo <gabriel.piles.glez@gmail.com>
2024-11-21 18:15:24 +0100

    Gliner first date when date between segments

--- src/trainable_entity_extractor/extractors/pdf_to_text_extractor/methods/GlinerFirstDateMethod.py
+++ src/trainable_entity_extractor/extractors/pdf_to_text_extractor/methods/GlinerFirstDateMethod.py
@@ -1,6 +1,6 @@
 import re
-from time import time
 
+from trainable_entity_extractor.data.PdfDataSegment import PdfDataSegment
 from trainable_entity_extractor.extractors.pdf_to_text_extractor.methods.FirstDateMethod import FirstDateMethod
 from trainable_entity_extractor.extractors.text_to_text_extractor.methods.GlinerDateParserMethod import (
     GlinerDateParserMethod,
@@ -13,9 +13,9 @@ class GlinerFirstDateMethod(FirstDateMethod):
         year_pattern = re.compile(r"([0-9]{2})")
         return bool(year_pattern.search(text.replace(" ", "")))
 
-    def get_date_from_segments(self, segments, languages):
-        for segment in self.loop_segments(segments):
-
+    def get_date_from_segments(self, segments: list[PdfDataSegment], languages):
+        segments_for_dates = self.merge_segments_for_dates(segments)
+        for segment in self.loop_segments(segments_for_dates):
             if not self.contains_year(segment.text_content):
                 continue
 
@@ -25,3 +25,24 @@ class GlinerFirstDateMethod(FirstDateMethod):
                 return date.strftime("%Y-%m-%d")
 
         return ""
+
+    def merge_segments_for_dates(self, segments):
+        min_words = 25
+        merge_segments: list[list[PdfDataSegment]] = list()
+        for segment in segments:
+            if not merge_segments:
+                merge_segments.append([segment])
+                continue
+
+            words_previous_segment = self.count_segments_words(merge_segments[-1])
+
+            if words_previous_segment < min_words:
+                merge_segments[-1].append(segment)
+
+            merge_segments.append([segment])
+        segments_for_dates = [PdfDataSegment.from_list_to_merge(segments) for segments in merge_segments]
+        return segments_for_dates
+
+    @staticmethod
+    def count_segments_words(segments: list[PdfDataSegment]):
+        return sum([len(segment.text_content.split()) for segment in segments])
