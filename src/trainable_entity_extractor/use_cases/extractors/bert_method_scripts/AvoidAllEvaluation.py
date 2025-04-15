d5ab628c2f9005940421ace8173289719ee56cdc
Gabo <gabriel.piles.glez@gmail.com>
2025-04-15 13:27:15 +0200

    Balance no label data

--- src/trainable_entity_extractor/use_cases/extractors/bert_method_scripts/AvoidAllEvaluation.py
+++ src/trainable_entity_extractor/use_cases/extractors/bert_method_scripts/AvoidAllEvaluation.py
@@ -5,7 +5,7 @@ class AvoidAllEvaluation(DefaultFlowCallback):
     def on_step_end(self, args, state, control, **kwargs):
         super().on_step_end(args, state, control, **kwargs)
 
-        if state.global_step <= 8000:
+        if state.global_step <= 88000:
             control.should_evaluate = False
 
         return control
