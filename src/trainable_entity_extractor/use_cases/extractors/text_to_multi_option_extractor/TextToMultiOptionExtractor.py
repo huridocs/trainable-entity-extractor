9076d9ea2e3627d27525661f3703eb43310b4e35
Gabo <gabriel.piles.glez@gmail.com>
2025-06-09 19:42:21 +0200

    Update train test split for text to multioption

--- src/trainable_entity_extractor/use_cases/extractors/text_to_multi_option_extractor/TextToMultiOptionExtractor.py
+++ src/trainable_entity_extractor/use_cases/extractors/text_to_multi_option_extractor/TextToMultiOptionExtractor.py
@@ -241,41 +241,51 @@ class TextToMultiOptionExtractor(ExtractorBase):
         samples_without_values = sum(1 for samples in samples_by_labels.values() if not samples)
         return (samples_without_values / total_samples) * 100
 
-    @staticmethod
-    def get_train_test_sets_using_labels(
-        samples_by_labels: dict[str, list[TrainingSample]]
-    ) -> (list[TrainingSample], list[TrainingSample]):
-        test_set = set()
-        all_samples = {sample for samples in samples_by_labels.values() for sample in samples}
-        sorted_labels_by_samples_count = sorted(samples_by_labels.keys(), key=lambda x: len(samples_by_labels[x]))
-
-        for label in sorted_labels_by_samples_count:
-            test_set.update(samples_by_labels[label])
-            if len(test_set) / len(all_samples) >= 0.10:
-                break
-        if len(all_samples) - len(test_set) < 8:
-            test_set = set(list(all_samples)[: int(len(all_samples) * 0.30)])
-        else:
-            test_size = int(len(all_samples) * 0.10)
-            test_set.update(list(all_samples)[:test_size])
-
-        train_set = all_samples - test_set
-        return list(train_set), list(test_set)
+    # @staticmethod
+    # def get_train_test_sets_using_labels(
+    #     samples_by_labels: dict[str, list[TrainingSample]]
+    # ) -> (list[TrainingSample], list[TrainingSample]):
+    #     test_set = set()
+    #     all_samples = {sample for samples in samples_by_labels.values() for sample in samples}
+    #     sorted_labels_by_samples_count = sorted(samples_by_labels.keys(), key=lambda x: len(samples_by_labels[x]))
+    #
+    #     for label in sorted_labels_by_samples_count:
+    #         test_set.update(samples_by_labels[label])
+    #         if len(test_set) / len(all_samples) >= 0.10:
+    #             break
+    #     if len(all_samples) - len(test_set) < 8:
+    #         test_set = set(list(all_samples)[: int(len(all_samples) * 0.30)])
+    #     else:
+    #         test_size = int(len(all_samples) * 0.10)
+    #         test_set.update(list(all_samples)[:test_size])
+    #
+    #     train_set = all_samples - test_set
+    #     return list(train_set), list(test_set)
 
     @staticmethod
     def get_stats(extraction_data):
-        options = Counter()
+        options_with_samples = Counter()
         for sample in extraction_data.samples:
-            options.update([option.label for option in sample.labeled_data.values])
+            options_with_samples.update([option.label for option in sample.labeled_data.values])
+
         languages = Counter()
         for sample in extraction_data.samples:
             languages.update([sample.labeled_data.language_iso])
 
+        all_option_labels = {opt.label for opt in extraction_data.options}
+        options_with_no_samples = all_option_labels - set(options_with_samples.keys())
+
         options_count = len(extraction_data.options)
         stats = f"\nNumber of options: {options_count}\n"
         stats += f"Number of samples: {len(extraction_data.samples)}\n"
+
         stats += f"Languages\n"
         stats += "\n".join([f"{key} {value}" for key, value in languages.most_common()])
-        stats += f"\nOptions\n"
-        stats += "\n".join([f"{key} {value}" for key, value in options.most_common()])
+        stats += f"\nOptions with samples\n"
+        stats += "\n".join([f"{key} {value}" for key, value in options_with_samples.most_common()])
+
+        if options_with_no_samples:
+            stats += f"\nOptions with no samples\n"
+            stats += "\n".join(list(options_with_no_samples))
+
         return stats
