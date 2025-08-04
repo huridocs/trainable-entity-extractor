e4d19e199632930f3cf58f41b05c340444796715
Gabo <gabriel.piles.glez@gmail.com>
2025-08-04 14:16:47 +0200

    [skip ci] Remove propagate false for logging

--- src/trainable_entity_extractor/config.py
+++ src/trainable_entity_extractor/config.py
@@ -21,4 +21,3 @@ if GRAYLOG_IP:
 logging.root.handlers = []
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=handlers)
 config_logger = logging.getLogger(__name__)
-config_logger.propagate = False
